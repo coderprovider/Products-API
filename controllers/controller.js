d2d0b2cd1262120d4edb7548bf617da5e7f1774f
whitefury01 <white.pury1114@gmail.com>
2018-09-18 10:07:21 -0600

    Tweaked a few items for consistency between server and docs

--- controllers/controller.js
+++ controllers/controller.js
@@ -11,7 +11,7 @@ module.exports = {
                 cart: [],
                 products: cloneDeep(products)
             }
-        return res.send({apiKey})
+        return res.send({key: apiKey})
     },
 
     requireApiKey: (req, res, next) => {
